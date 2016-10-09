53acf94069f7fa80c20716eb6c6570f251b71535
SeannaBirchwood <chelsea.craig1223@gmail.com>
2016-10-09 20:31:40 +0000

    library and loadeddie incomplete, having issues returning values from arrays and objects

---
+++ loadedDieAgain.js
@@ -0,0 +1,18 @@
+var loadedDie = (function() {
+	var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+	var listLength = list.length
+
+		return function () {
+	var i = 0;
+	//list.forEach(function(elm) {
+		//if (elm <= list.length);
+  for (var i = 0; i < listLength; i++) {
+  	 return i += 1
+  	}
+
+}
+});
+
+console.log(loadedDie());
+
+//incomplete
